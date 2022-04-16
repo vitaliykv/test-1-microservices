ed94fe9ba4f07fd2f8548ce858e4363db22a156a
vitaliykv <vitalii.kv@gmail.com>
2022-04-17 00:59:24 +0300

    add ocelot.json

--- src/Gateways/Gateways/Program.cs
+++ src/Gateways/Gateways/Program.cs
@@ -1,5 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+builder.Services.AddOcelot();
+
+
 var app = builder.Build();
+
+app.UseOcelot().Wait();
 
 app.MapGet("/", () => "Hello World!");
 
