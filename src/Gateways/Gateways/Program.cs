8ca8091c0fc43966218c4fd9f19c2b674c78f248
vitaliykv <vitalii.kv@gmail.com>
2022-04-20 23:42:05 +0300

    ocelot-compose

--- src/Gateways/Gateways/Program.cs
+++ src/Gateways/Gateways/Program.cs
@@ -3,7 +3,10 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json");
+var ocelot_json_file = builder.Configuration.GetValue<string>("Ocelot_json");
+//ocelot-compose.json
+// Ocelot_json
+builder.Configuration.AddJsonFile(ocelot_json_file);
 
 builder.Services.AddOcelot();
 
